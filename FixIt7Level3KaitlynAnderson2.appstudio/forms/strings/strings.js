06ad97c3c5ccf4b96d31a77197f3aa1aec9f6631
cnc55808 <71236348+cnc55808@users.noreply.github.com>
2020-10-14 10:32:42 -0500

    revision

--- FixIt7Level3KaitlynAnderson2.appstudio/forms/strings/strings.js
+++ FixIt7Level3KaitlynAnderson2.appstudio/forms/strings/strings.js
@@ -1,24 +1,23 @@
 let quoteString = ("Failure is simply the opportunity to begin again this time more intelligently.")
 let upperCaseString = quoteString.toUpperCase(quoteString)
-console.log(`Upper case string is: ${upperCaseString}`)
+  alert(`Upper case string is: ${upperCaseString}`)
 
 let authorString = (`-Henry Ford`)
 let completeString = (quoteString + authorString)
 let found = completeString.includes("Henry")
- console.log(found)
 if (found = true)
-  console.log("Henry was in the quote string.")
+  alert("Henry was in the quote string.")
 else
- console.log("Henry was not in the quote string.")
-
-
-//TO LOWERCASE
+ alert("Henry was not in the quote string.")
+ 
+ 
+ 
 secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
 lowerCaseString = secondQuote.toLowerCase(secondQuote)
-console.log(`Lower case string is: ${lowerCaseString}`)
+  alert(`Lower case string is: ${lowerCaseString}`)
 
 
-//ADDS 10 to user input number
+
 userNumber = prompt("Please enter a number.")
-addTen = +userNumber + 10 //use +userNumber to turn the string into a number so it doesn't just concatinate 
-console.log(`The number ${userNumber}, + 10 is ${addTen}`)
+addTen = +userNumber + 10 
+  alert(`The number ${userNumber}, + 10 is ${addTen}`)
--- Fixit7CarleeCline.appstudio/forms/for/for.js
+++
@@ -1,5 +0,0 @@
-let names = ["Bob", "Janet", "Tom", "Bob", "Randy", "Elizabeth", "Kim", "Nancy"]
-
-for (i = 0; i <= names.length - 1; i++) {
-  console.log(names[i])
-  }
